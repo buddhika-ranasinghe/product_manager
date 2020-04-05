5df00301390cba819ca9857199514d5e21fe7e56
Buddhika Ranasinghe <buddhikainarush@gmail.com>
2020-04-05 21:55:27 +0530

    Initial Commit - Re Commit with Angular CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector : 'pm-root',
-  	templateUrl : './app.component.html',
-  	styleUrls : ['./app.component.css']
-  
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
-export class AppComponent{
-	title: string = "Page Title";
+export class AppComponent {
+  title = 'product-manager';
 }
